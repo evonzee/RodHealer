da78ecbb8dbe72765d78783ad8da882bb1bb2c89
Ancientgods <devoogdlaurens2@hotmail.com>
2014-06-24 00:06:08 +0200

    Initial Commit

---
+++ Rodhealer/main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Terraria;
+using TShockAPI;
+using TerrariaApi.Server;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace RODhealer
+{
+    [ApiVersion(1, 16)]
+    public class Rodhealer : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public override string Author
+        {
+            get { return "Ancientgods"; }
+        }
+
+        public override string Name
+        {
+            get { return "Rod healer"; }
+        }
+
+        public override string Description
+        {
+            get { return "Heals you when using the rod of discord!"; }
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+
+            base.Dispose(disposing);
+        }
+
+        public Rodhealer(Main game)
+            : base(game)
+        {
+            Order = 1;
+        }
+
+        private void OnGetData(GetDataEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            if (e.MsgID == PacketTypes.Teleport)
+            {
+                TSPlayer player = TShock.Players[e.Msg.whoAmI];
+                if (player != null)
+                {
+                    if (player.Group.HasPermission("tshock.tp.rod.nodamage"))
+                    {
+                        List<int> buffs = new List<int>(player.TPlayer.buffType);
+                        if (buffs.Contains(88))
+                            player.Heal(player.FirstMaxHP / 6);
+                    }
+                }
+            }
+        }
+    }
+}
